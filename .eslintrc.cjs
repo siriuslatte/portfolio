5b79abdf0416adfece84bad01450f3d60734b0ac
Ademir Sánchez <ademir.correopersonal@gmail.com>
2024-07-03 13:56:28 -0600

    eslint: add more rules for react

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,15 +3,21 @@
 const config = {
   "parser": "@typescript-eslint/parser",
   "parserOptions": {
-    "project": true
+    "project": true,
+    "ecmaFeatures": {
+      "jsx": true
+    }
   },
   "plugins": [
-    "@typescript-eslint"
+    "@typescript-eslint",
+    "react"
   ],
   "extends": [
     "next/core-web-vitals",
     "plugin:@typescript-eslint/recommended-type-checked",
-    "plugin:@typescript-eslint/stylistic-type-checked"
+    "plugin:@typescript-eslint/stylistic-type-checked",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime"
   ],
   "rules": {
     "@typescript-eslint/array-type": "off",
@@ -37,7 +43,45 @@ const config = {
           "attributes": false
         }
       }
-    ]
+    ],
+    "react/prefer-stateless-function": "error",
+    "react/button-has-type": "error",
+    "react/no-unused-prop-types": "error",
+    "react/jsx-pascal-case": "error",
+    "react/jsx-no-script-url": "error",
+    "react/no-children-prop": "error",
+    "react/no-danger": "error",
+    "react/no-danger-with-children": "error",
+    "react/no-unstable-nested-components": ["error", { allowAsProps: true }],
+    "react/jsx-fragments": "error",
+    "react/destructuring-assignment": [
+      "error",
+      "always",
+      { destructureInSignature: "always" },
+    ],
+    "react/jsx-no-leaked-render": ["error", { validStrategies: ["ternary"] }],
+    "react/jsx-max-depth": ["error", { max: 5 }],
+    "react/function-component-definition": [
+      "warn",
+      { namedComponents: "arrow-function" },
+    ],
+    "react/jsx-key": [
+      "error",
+      {
+        checkFragmentShorthand: true,
+        checkKeyMustBeforeSpread: true,
+        warnOnDuplicates: true,
+      },
+    ],
+    "react/jsx-no-useless-fragment": "warn",
+    "react/jsx-curly-brace-presence": "warn",
+    "react/no-typos": "warn",
+    "react/display-name": "warn",
+    "react/self-closing-comp": "warn",
+    "react/jsx-sort-props": "warn",
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-one-expression-per-line": "off",
+    "react/prop-types": "off",
   }
 }
 module.exports = config;
